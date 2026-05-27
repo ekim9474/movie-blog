3c73e812aafc73d1696515f6d2ed7f64a5e483b1
ekim9474 <elliotkim9474@gmail.com>
2026-05-27 01:45:03 -0700

    Fixed a href refs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ export default defineConfig({
     provider: fontProviders.fontsource(),
     name: "Lato",
     cssVariable: "--font-lato",
-  }]
+  }],
+  site: 'https://https://ekim9474.github.io',
+  base: '/movie-blog'
 });
 
