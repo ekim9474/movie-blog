e4a10f4ab8c8485d335479c7acab62e8735335dd
ekim9474 <elliotkim9474@gmail.com>
2026-05-31 22:48:41 -0700

    Removed broken pages + changed loader to live GitHub Pages config

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,9 +10,9 @@ import { z } from 'astro/zod';
 // 4. Define a `loader` and `schema` for each collection
 const ratingsCollection = defineCollection({
   // uncomment this for dev environment testing
-  loader: glob({ pattern: "**/*.md", base: "./src/pages/" }),
+  // loader: glob({ pattern: "**/*.md", base: "./src/pages/" }),
   // works for live GitHub Pages
-  // loader: glob({ pattern: "**/*.md", base: "./src/pages/ratings" }),
+  loader: glob({ pattern: "**/*.md", base: "./src/pages/ratings" }),
 });
 
 // 5. Export a single `collections` object to register your collection(s)
