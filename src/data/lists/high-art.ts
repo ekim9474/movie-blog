8f3b484cb6e90b0bd4c4f43364fa5261382f297b
ekim9474 <elliotkim9474@gmail.com>
2026-08-20 23:31:26 -0700

    Added movie lists

---
+++ src/data/lists/high-art.ts
@@ -0,0 +1,15 @@
+export const highArtList = {
+    1: {"movieName": `Blue`, "year": "1993", "director": `Derek Jarman`, "runtime": "1hr 19min", "country": "United Kingdom", "image": `https://images.elliotsmovieblog.com/blue.jpg`,
+        "description": `Blue documents Jarman's journey towards death with AIDS. 
+        Through the domination of color and narration, 
+        Jarman creates an intensely intimate and personal story.`},
+    2: {"movieName": `Jeanne Dielman, 23 quai du Commerce, 1080 Bruxelles`, "year": "1975", "director": `Chantal Akerman`, "runtime": "3hr 21min", "country": "France", "image": `https://images.elliotsmovieblog.com/jdiel1.jpg`,
+        "description": `Widely considered to be Akerman's most notable work, 
+        Jeanne Dielman is a claustrophobic and unfettered deconstruction of domestic servitude.`},
+    3: {"movieName": `Evolution of a Filipino Family`, "year": "2004", "director": `Lav Diaz`, "runtime": "10hr 25min", "country": "Philippines", "image": `https://images.elliotsmovieblog.com/evolfilipinofam.png`,
+        "description": `Evolution of a Filipino Family is a 10 hour epic 
+        that focuses on a farming family under the Marcos regime.`},
+    4: {"movieName": `Blood of a Poet`, "year": "2004", "director": `Jean Cocteau`, "runtime": "55min", "country": "France", "image": `https://images.elliotsmovieblog.com/bloodofpoet.jpg`,
+        "description": `As Cocteau's first feature film, Blood of a Poet experiments 
+        with a visual language that poetically captures the mind of an artist.`},
+}
